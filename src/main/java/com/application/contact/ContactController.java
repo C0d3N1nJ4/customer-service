c0161dd04cc64d0f217815fb76308647574508f9
naiomi <Naiomi.Naidoo@icloud.com>
2024-03-17 20:31:33 +0100

    Refactoring project

--- src/main/java/com/application/contact/ContactController.java
+++ src/main/java/com/application/contact/ContactController.java
@@ -1,6 +1,11 @@
 package com.application.contact;
 
+import com.application.address.Address;
 import com.application.exceptions.ContactNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,17 +17,30 @@ public class ContactController {
 
     private final ContactServiceImpl contactService;
 
-
     public ContactController(ContactServiceImpl contactService) {
         this.contactService = contactService;
     }
 
     @GetMapping
+    @Operation(summary = "Retrieve contacts", responses = {
+            @ApiResponse(description = "Successful Operation",
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Contact.class)))
+    })
     public List<Contact> getContacts() {
         return contactService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retrieve contact by id", responses = {
+            @ApiResponse(description = "Successful Operation",
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Contact.class)))
+    })
     public Contact getContactById(@PathVariable String id) {
         if (contactService.existsById(id)) {
             return contactService.findById(id);
@@ -32,11 +50,16 @@ public class ContactController {
     }
 
     @PostMapping
-    @ResponseBody
+    @Operation(summary = "Create a new contact", responses = {
+            @ApiResponse(description = "Successful Operation",
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Contact.class)))
+    })
     @ResponseStatus(HttpStatus.CREATED)
     public Contact createContact(@RequestBody Contact contact) {
         return contactService.create(contact);
     }
-
 
 }
